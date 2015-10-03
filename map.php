bdf38aee2409507b4a0d40d6dec921b7037ac7ee
Finjon Kiang <kiange@gmail.com>
2015-10-03 14:04:08 +0800

    apply design from map_n

--- map.php
+++ map.php
@@ -4,24 +4,80 @@
         <title>Geocoding service</title>
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
         <meta charset="utf-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css" />
         <style>
             html, body {
                 height: 100%;
                 margin: 0;
                 padding: 0;
             }
+            #map {
+                height: 500px;
+            }
+            .item_control{
+                text-align: right;
+                padding:6px 0 0;
+            }
+            .item_control span{
+                margin-left:6px;
+            }
         </style>
     </head>
     <body>
-        <div id="map" style="height: 500px; width: 800px; float: left;"></div>
-        <div style="float: left;">
-            <input id="address" type="textbox" value="">
-            <input id="submit" type="button" value="搜尋">
-            <hr />
-            <br />經度：<input id="longtitude" type="textbox" value="">
-            <br />緯度：<input id="latitude" type="textbox" value="">
+        <div class="container">
+            <div class="row">
+                <h1>所有資料</h1>
+                <div class="col-md-8">
+                    <div id="map"></div>
+                </div><!-- col-md-8 -->
+                <div class="col-md-4">
+                    <div class="col-md-12">
+                        <input id="address" type="textbox" value="">
+                        <input id="submit" type="button" value="搜尋">
+                        <hr />
+                        <br />經度：<input id="longtitude" type="textbox" value="">
+                        <br />緯度：<input id="latitude" type="textbox" value="">
 
-        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="list-group" id="relative_location">
+                        <a class="list-group-item" data-sn="0">
+                            <h4 class="list-group-item-heading">台南市政府_0</h4>
+                            <p class="list-group-item-text">台南行政中心</p>
+                            <div class="item_control">
+                                <span data-toggle="tooltip" data-placement="top" title="合併" class="glyphicon glyphicon-pushpin btn btn-primary"></span>
+                                <span data-toggle="tooltip" data-placement="top" title="刪除" class="glyphicon glyphicon-trash btn btn-danger"></span>
+                            </div>
+                        </a>
+                        <a class="list-group-item" data-sn="0">
+                            <h4 class="list-group-item-heading">台南市政府_0</h4>
+                            <p class="list-group-item-text">台南行政中心</p>
+                            <div class="item_control">
+                                <span data-toggle="tooltip" data-placement="top" title="合併" class="glyphicon glyphicon-pushpin btn btn-primary"></span>
+                                <span data-toggle="tooltip" data-placement="top" title="刪除" class="glyphicon glyphicon-trash btn btn-danger"></span>
+                            </div>
+                        </a>
+                        <a class="list-group-item" data-sn="0">
+                            <h4 class="list-group-item-heading">台南市政府_0</h4>
+                            <p class="list-group-item-text">台南行政中心</p>
+                            <div class="item_control">
+                                <span data-toggle="tooltip" data-placement="top" title="合併" class="glyphicon glyphicon-pushpin btn btn-primary"></span>
+                                <span data-toggle="tooltip" data-placement="top" title="刪除" class="glyphicon glyphicon-trash btn btn-danger"></span>
+                            </div>
+                        </a>
+                        <a class="list-group-item" data-sn="0">
+                            <h4 class="list-group-item-heading">台南市政府_0</h4>
+                            <p class="list-group-item-text">台南行政中心</p>
+                            <div class="item_control">
+                                <span data-toggle="tooltip" data-placement="top" title="合併" class="glyphicon glyphicon-pushpin btn btn-primary"></span>
+                                <span data-toggle="tooltip" data-placement="top" title="刪除" class="glyphicon glyphicon-trash btn btn-danger"></span>
+                            </div>
+                        </a>
+                    </div>
+                </div><!-- col-md-4 -->
+            </div><!-- row -->
+        </div><!-- container -->
+
         <script>
             var markers = [];
 
@@ -68,7 +124,6 @@
                         google.maps.event.addListener(marker, 'dragend', function (e) {
                             elementLat.val(e.latLng.lat());
                             elementLng.val(e.latLng.lng());
-                            $('#address').val(e.latLng.toString());
                         });
                         elementLng.val(results[0].geometry.location.lng());
                         elementLat.val(results[0].geometry.location.lat());
